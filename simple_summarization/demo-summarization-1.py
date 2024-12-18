456cf267e6412c83cf2508dca27f584765759227
gsilva2016 <g2020sudo@none.com>
2024-12-17 19:58:10 -0700

    draft 1

--- simple_summarization/demo-summarization-1.py
+++ simple_summarization/demo-summarization-1.py
@@ -1,5 +1,6 @@
 import argparse
 import time
+import os
 from langchain_huggingface import HuggingFacePipeline
 from langchain_community.document_loaders import OpenVINOSpeechToTextLoader
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
@@ -11,6 +12,7 @@ from langchain_core.documents import Document
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_text_splitters import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+import ast
 
 MAX_NEW_TOKENS = 8040
 
@@ -28,113 +30,134 @@ print("LLM model_id: ", args.model_id)
 print("ASR model_id: ", args.asr_model_id)
 print("Inference device  : ", args.device)
 print("Audio file: ", args.audio_file)
-input("Press Enter to continue...")
+#input("Press Enter to continue...")
 # 
+
+start_time = time.time()
 
 # Edge local Speech-to-Text: https://huggingface.co/OpenVINO/distil-whisper-tiny-int4-ov
 # Note: Requires LangChain patch
 asr_loader = OpenVINOSpeechToTextLoader(args.audio_file, args.asr_model_id, device=args.device)
 docs = asr_loader.load()
+print("ASR docs created: ", len(docs))
+#from langchain_core.load import dumpd, dumps, load, loads
 #print("ARS results: ", docs)
-
-
+#with open('LowerPrimary-Speakandwrite-transcript-asdoc.txt', 'w') as f:
+#    f.write(str(docs))
+#quit()
+#with open('LowerPrimary-Speakandwrite-transcript-asdoc.txt', 'r') as f:
+#    text = f.read()
+#    print("text: ", text)
+#    docs = load(text)
+#    docs = ast.literal_eval(text)
+#    print(docs)
+#quit()
 # Edge local LLM inference for text summarization
 print("Starting LLM inference...")
-ov_config = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": ""}
-#"num_return_sequences"
-# temp: 0.2
-#  "encoder_repetition_penalty": 1.5
+model_cache_available = os.path.exists("./" + str(args.model_id))
+ov_config = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": "./cache-ov-models"}
 ov_llm = HuggingFacePipeline.from_model_id(
     model_id=args.model_id,
     task="text-generation",
     backend="openvino",
-    model_kwargs={"device": "CPU", "ov_config": ov_config}, # used for invoke
-    pipeline_kwargs={"max_new_tokens": MAX_NEW_TOKENS, "do_sample": True, "top_k": 10, "temperature": 1.0, "return_full_text": True, "repetition_penalty": 1.0, "encoder_repetition_penalty": 1.0, "use_cache": True},
+    model_kwargs={"device": args.device, "ov_config": ov_config, "export": True if not model_cache_available else False}, # used for invoke
+    pipeline_kwargs={"max_new_tokens": MAX_NEW_TOKENS, "do_sample": True, "top_k": 10, "temperature": 1.0, "return_full_text": False, "repetition_penalty": 1.0, "encoder_repetition_penalty": 1.0, "use_cache": True},
 )
-
 ov_llm.pipeline.tokenizer.pad_token_id = ov_llm.pipeline.tokenizer.eos_token_id
-
-template = """Your task is to improve the user input's readability by adding punctuation if needed, removing verbal tics, and structure the text in paragraphs separated with '\n\n' for the following: {text}.
-Keep the wording as faithful as possible to the original text. 
-Put your answer within <answer></answer> tags."""
-
-template = """system: Identify all the relevant sections/topics in the following text: {context}
-For each section identified  Create a Section Title and Create a Section Summary and keep the  wording as faithful as possible to the original text"""
-
-template = """
-You are a helpful assistant.
-
-Keep the wording as faithful as possible to the original text. Create all relevant sections, section titles, and paraphrased section summaries for the following text: 
-
-{context}"""
-
-template = """
-Create concise sections of the main topics. Create all relevant sections, section titles, and paraphrased section summaries. Keep the wording as faithful as possible to the original text provided by the user/human.
-
-Human: Generate concise sections for the main topics in: {context}"""
-
-#template = """
-#Create consise sections of the main topics from the text provided by the user/human. For each section you create give title as well. Keep the wording as faithful as possible to the original text provided by the user/human. Do not add any information that is not provided by the human/user.
-#"""
-
-#template = """You are a helpful assistant.
-
-#Your task is to structure a given text into paragraphs. Keep the wording as close as possible to the original text. 
-
-#Format your result as a list of paragraphs.
-#Example:
-#1. I am a paragraph.
-#2. I am another paragraph. So on, and so forth. 
-#Keep creating paragraphs like this.
-#3. This is yet another paragraph!
-
-#User: Format the user's input by structuring the provided text into paragraphs for the following: 
-
-#{context}
-
-#"""
-
-prompt = ChatPromptTemplate.from_messages([("system", template)])
-chain = create_stuff_documents_chain(ov_llm, prompt)
+if not os.path.exists("./" + str(args.model_id)):
+    ov_llm.pipeline.model.save_pretrained("./" + str(args.model_id))
+    ov_llm.pipeline.tokenizer.save_pretrained("./" + str(args.model_id))
+else:
+    print("Using OpenVINO model cache")
 
 print("-------LLM Chapter Summary Generation---------")
-start_time = time.time()
-
 # load ASR or text file for debugging
-text = docs_loader.format_docs(docs)
-
+#text = docs_loader.format_docs(docs)
+#with open('LowerPrimary-Speakandwrite-transcript.txt', 'w') as f:
+#    f.write(text)
 #with open('transcript.txt', 'r') as f:
 #with open('mit_transcript.txt', 'r') as f:
+#with open('LowerPrimary-Speakandwrite-transcript.txt', 'r') as f:
 #    text = f.read()
 
 #print("-----LLM text input-----")
 #print(text)
 #print("Number of chars: ", str(len(text)))
 
-def transcript_to_paragraphs(transcript_as_text, chain, chunk_size=5000):
+def get_summaries(transcript, chain):
 
+    for i in range(0, len(transcript)):
+        doc_in = [transcript[i]]
+        text = docs_loader.format_docs(doc_in)
+
+        # sumllama
+        instruction = "Please summarize the input document."
+        row_json = [{"role": "user", "content": f"Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Input:\n{text}\n\n### Response:\n"}]        
+        formatted_prompt = ov_llm.pipeline.tokenizer.apply_chat_template(row_json, tokenize=False)
+        summary = ov_llm.invoke(formatted_prompt)
+        # remove LLM non-essential text
+        # assistant\n\n
+        summary = summary.replace('assistant\n\n', '')
+        transcript[i].metadata["summary"] = summary
+
+
+def get_chunked_docs(asr_docs, chunk_size=1000):
     text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size =chunk_size,
-            chunk_overlap  = 0,
-            length_function = len,
-            )
+                    chunk_size = chunk_size,
+                    chunk_overlap = 0,
+                    length_function = len,
+    )
 
-    doc_in = [Document(
-            page_content=transcript_as_text,
-            metadata={
-                "language": 'en'
-            })]
-    
-    split_docs = text_splitter.split_documents(doc_in)    
-    print(f"Generated {len(split_docs)} documents.")
-    for i in range(0, len(split_docs)):
-        doc_in = [split_docs[i]]
-        print("Input text:", doc_in)
-        paragraph_created = chain.invoke({"context": doc_in})
-        print("Paragraph/Section ", str(i), " created: \n", paragraph_created)
+    full_text = docs_loader.format_docs(asr_docs)
+    doc_in = [Document(page_content=full_text,
+        metadata={
+            "language": 'en',
+            "summary" : '',
+            "topic": '',
+            "start": '',
+            "end": ''
+        })]
+
+    # chunk the docs by CHUNK_SIZE
+    chunked_docs = text_splitter.split_documents(doc_in)
+    #print(f"Generated {len(chunked_docs)} document chunks.")
+
+    # fill chunked_docs with correct timestamps
+    asr_ts = ast.literal_eval(asr_docs[0].metadata["timestamp"])
+    chunk_idx = 0
+    chunk_len = 0
+    len_asr_docs = len(asr_docs)
+    for i in range(0, len_asr_docs):
+        chunk_len = chunk_len + len(asr_docs[i].page_content)
+        if chunk_len > chunk_size:
+            asr_ts_e = ast.literal_eval(asr_docs[i-1].metadata["timestamp"])
+            #print("update: ", asr_ts, "= ", asr_ts[0], " ... " , asr_ts_e) # " ", chunk_idx, " orig: ", chunked_docs[chunk_idx].metadata["timestamp"])
+            chunked_docs[chunk_idx].metadata["start"] = asr_ts[0]
+            chunked_docs[chunk_idx].metadata["end"]   = asr_ts_e[0]
+            #print("Updated chunked_doc: ", chunked_docs[chunk_idx])
+            #print('\n\n')
+
+            asr_ts = ast.literal_eval(asr_docs[i].metadata["timestamp"])
+            chunk_idx = chunk_idx + 1
+            chunk_len = chunk_len - chunk_size
+#    print('\n\n')
+#    print("Last chunk process: ", chunk_idx, " vs. ", len_asr_docs, " needed")
+#    print(asr_docs[len_asr_docs-1].metadata["timestamp"])
+#    print("Before: ", chunked_docs[chunk_idx])
+#    print('\n\n')
+    chunked_docs[chunk_idx].metadata["start"] = asr_ts_e[1]
+    asr_ts = ast.literal_eval(asr_docs[len_asr_docs-1].metadata["timestamp"])
+    chunked_docs[chunk_idx].metadata["end"] = "end" #asr_ts[1]
+#    chunked_docs[chunk_idx].metadata["start"] = asr_ts[0]
+#    print("After: ", chunked_docs[chunk_idx])
+
+    return chunked_docs
 
 
-transcript_to_paragraphs(text, chain, chunk_size=1000) #61000) #5000)
+docs = get_chunked_docs(docs, chunk_size=1000)
+get_summaries(docs, chain) #, chunk_size=1000) #61000) #5000)
+print("Summary Results\n")
+print(docs)
 end_time = time.time()
 print("\n\n")
 print("LLM inference took: ", end_time - start_time, " (seconds) / ", (end_time-start_time)/60, " (minutes)")
